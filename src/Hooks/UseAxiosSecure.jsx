4ddb5dd33d6351adaebfd9e32e2156781f796032
Mokim <mamokim2005@gmail.com>
2026-06-15 16:49:13 +0600

    rady to deploy

--- src/Hooks/UseAxiosSecure.jsx
+++ src/Hooks/UseAxiosSecure.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import UserAuth from "./UserAuth";
 
 const axiosSecure = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://city-fix-server-green.vercel.app",
 });
 
 const UseAxiosSecure = () => {
